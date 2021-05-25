1f9c02fe1a48e0df08b7949a86c49e2c853fa042
nana1243 <hieun1243@gmail.com>
2021-05-26 08:54:38 +0900

    fea) 캡슐화를 통해 결합도 낮추기

--- Object/src/chap1_update_2/Bag.java
+++ Object/src/chap1_update_2/Bag.java
@@ -20,12 +20,20 @@ public class Bag {
         this.amount = amount;
     }
 
+    public Long hold(Ticket ticket){
+        if(hasInvitation()){
+            setTicket(ticket);
+            return 0L;
+        }else{
+            setTicket(ticket);
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
+
 
     public boolean hasInvitation(){
         return invitation!=null;
-    }
-    public boolean hasTicket(){
-        return ticket !=null;
     }
 
     public void setTicket(Ticket ticket) {
@@ -36,8 +44,5 @@ public class Bag {
         this.amount -=amount;
     }
 
-    public void plusAmount(Long amount){
-        this.amount+=amount;
-    }
 
 }
